8026e29e5e95881cc4046d99c53df6e84bac4954
Jose Vega <jocvegar@gmail.com>
2022-06-04 18:13:41 -0600

    refresher

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+    <ion-content>
+      <ion-refresher
+        slot="fixed"
+        pull-factor="0.5"
+        pull-min="100"
+        pull-max="200"
+      >
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+    </ion-content>
     <nav-bar />
   </ion-app>
 </template>
