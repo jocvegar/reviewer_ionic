0f1dd241d9d889226425e83d3b1a4e160cc29fa4
Jose Vega <jocvegar@gmail.com>
2022-06-05 08:56:26 -0600

    remove refresher

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <ion-app>
     <ion-router-outlet />
-    <ion-content>
-      <ion-refresher
-        slot="fixed"
-        pull-factor="0.5"
-        pull-min="100"
-        pull-max="200"
-      >
-        <ion-refresher-content></ion-refresher-content>
-      </ion-refresher>
-    </ion-content>
     <nav-bar />
   </ion-app>
 </template>
